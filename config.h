005918ff4dafa574a224ee68ead938192ca71ead
Thomas Gies <thomas.gies@idmedia.com>
2020-09-08 15:51:43 +0200

    st tmux

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "tmux", NULL };
 static const char *dvolupcmd[] = { "/home/thomas/bin/volume", "2db+", NULL };
 static const char *dvoldowncmd[] = { "/home/thomas/bin/volume", "2db-", NULL };
 static const char *dvolmutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
