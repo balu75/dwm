5c2016055a01d1f6ad3004dfb66f7c7eed92e1f2
Thomas Gies <thomas.gies@gmx.de>
2022-09-30 13:49:43 +0200

    Font geändert

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Dejavu Sans Mono:size=9", "icons:size=10", "fontawesome:size=10" };
-static const char dmenufont[]       = "Dejavu Sans Mono:size=10";
+static const char *fonts[]          = { "Inconsolata Nerd Font Mono", "icons:size=10", "fontawesome:size=10" };
+static const char dmenufont[]       = "Inconsolata Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#504945";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
